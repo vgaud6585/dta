2ef8e48f6c05132875c450e471054aae4c84bd9e
Vishal gaud 9 <vgaud6585@gmail.com>
2026-01-09 11:01:45 +0530

    initial commit 3

--- Vishal/settings.py
+++ Vishal/settings.py
@@ -117,7 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 STATIC_ROOT = BASE_DIR / "stat"
 STATIC_URL = 'static/'
-STATICFILES_DIRS =[BASE_DIR / "static",] 
+STATICFILES_DIRS =[BASE_DIR / "static",BASE_DIR / "stat"] 
+#STATICFILES_DIRS =[BASE_DIR / "static",BASE_DIR / "stat"] 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
